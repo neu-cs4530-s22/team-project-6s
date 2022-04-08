954eb0629d5d45377fcc4bf798b6a00d4437bfe0
Lisa <yamada.l@northeastern.edu>
2022-04-08 18:30:05 -0400

    Frontend chat enabled when player is near another

---
+++ frontend/src/classes/Chat.ts
@@ -0,0 +1,30 @@
+import { nanoid } from 'nanoid';
+import Player from "./Player";
+
+export const CHAT_RADIUS = 80;
+
+export default class Chat {
+    public location: ChatLocation;
+  
+    private readonly _id: string;
+  
+    public occupantsByID: string[]
+  
+    constructor(id: string, occupantsByID: string[], location: ChatLocation) {
+        this._id = id;
+        this.occupantsByID = occupantsByID;
+        this.location = location;
+    }
+  
+    static fromServerChat(chatFromServer: ServerChat): Chat {
+      return new Chat(chatFromServer._id, chatFromServer._occupantsByID, chatFromServer.location);
+    }
+  }
+  export type ServerChat = { _id: string, _occupantsByID: string[], location: ChatLocation };
+  
+  export type ChatLocation = {
+    x: number;
+    y: number;
+    radius: number;
+  };
+  
