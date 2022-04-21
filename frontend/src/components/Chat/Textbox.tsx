166c122121909dd1f69f7d1b32d87c6b99960735
shifisingh <63437421+shifisingh@users.noreply.github.com>
2022-04-21 14:45:56 -0400

    uploadFiles functionality

--- frontend/src/components/Chat/Textbox.tsx
+++ frontend/src/components/Chat/Textbox.tsx
@@ -106,7 +106,7 @@ export default function Textbox(): JSX.Element {
           }
         </div>
       </div>
-      {/* <UploadFiles /> */}
+      {/<UploadFiles />/}
       </VStack>
       </>
     );
